05eefacaee482827ebf864aa71d69ffe092a918f
dipsywong98 <facebookarmyoffcal@gmail.com>
2017-09-28 13:16:38 +0800

    Q4 done

--- assignment.ml
+++ assignment.ml
@@ -49,32 +49,54 @@ fun insert_enroll (s:(int * string list),E([])) = E([s])
 (*Q3*)
 fun query_students (course_id,E(L)) =
     let 
-        fun Exist (course_id, []) = false
-        |   Exist (course_id, h::t) = if (course_id = h) then true else Exist(course_id, t);
+        fun Exist__ (course_id, []) = false
+        |   Exist__ (course_id, h::t) = if (course_id = h) then true else Exist__ (course_id, t);
 
         fun push_students (course_id,E([]),[]) = []
         |   push_students (course_id,E([]),students) = students
         |   push_students (course_id,E(h::t),students) = 
-                if( Exist(course_id, #2 h)) then push_students (course_id,E(t),students @ [#1 h])
+                if( Exist__ (course_id, #2 h)) then push_students (course_id,E(t),students @ [#1 h])
                 else push_students (course_id,E(t),students);
     in
         push_students(course_id,E(L),[])
     end;
+
+(*Q4*)
+fun IntMatch a b = a=b;
+fun count_distinct_students (prof, C(c), E(e)) =
+    let fun Helper (prof, C([]), E(_), students_list) = students_list
+    |   Helper (prof, C(_), E([]), students_list) = students_list 
+    |   Helper (prof, C(h::t), E(e), students_list) = 
+        if ( #2 h = prof) then Helper (prof, C(t), E(e), AppendUnique(query_students(#1 h, E(e)), students_list, IntMatch))
+        else Helper (prof, C(t), E(e), students_list)
+    in Helper (prof, C(c), E(e), []) end;
+
 
 print("....(Q1)....");
 insert_course ([], C [("comp10", "p01")]);
 insert_course ([("comp12", "p02")], C []);
 insert_course ([("comp10", "p02")], C [("comp10", "p01")]);
 insert_course ([("comp13", "p01"), ("comp12", "p02")], C [("comp10","p01")]);
+
 print("....(Q2)....");
  insert_enroll ((1702, []), E [(1701, ["comp10", "comp11"])]);
  insert_enroll ((1701, ["comp10", "comp11"]), E []);
  insert_enroll ((1701, ["comp11", "comp10"]), E [(1701, ["comp10",
 "comp12"])]);
 insert_enroll ((1702, ["comp10"]), E [(1701, ["comp10", "comp11"])]);
+
 print("....(Q3)....");
 query_students ("comp10", E []);
 query_students ("comp10", E [(1701, ["comp10"])]);
 query_students ("comp11", E [(1701, ["comp10"])]);
 query_students ("comp10", E [(1701, ["comp10", "comp11"]), (1702,
 ["comp13", "comp10"]), (1703, [])]);
+
+print("....(Q4)....");
+count_distinct_students ("p01", C [], E []);
+count_distinct_students ("p01", C [("comp10", "p01")], E []);
+count_distinct_students ("p01", C [("comp10", "p01")], E [(1701,
+["comp10"]), (1702, ["comp13"])]);
+count_distinct_students ("p01", C [("comp10", "p01"), ("comp12",
+"p02"), ("comp13", "p01")], E [(1701, ["comp10", "comp11"]), (1702,
+["comp13"])]);
